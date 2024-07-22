5a61a800d2761a98000aa2d3938be2fb7918aacf
jeseok <jeseok@dblab.postech.ac.kr>
2024-07-22 01:30:09 +0000

    Create reproduce scripts

---
+++ end-to-end_comparison.sh
@@ -0,0 +1,5 @@
+cd scripts||exit 
+python optimize.py --config=config_sysbenchrw.ini
+python optimize.py --config=config_sysbenchwo.ini
+python optimize.py --config=config_sysbenchro.ini
+python optimize.py --config=config_twitter.ini
