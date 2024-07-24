b4b9b33d3519433f43e79fe3df8681bb2e7d12ae
jeseok <jeseok@dblab.postech.ac.kr>
2024-07-24 23:06:15 +0000

    Twitter server

--- autotune/pipleline/pipleline.py
+++ autotune/pipleline/pipleline.py
@@ -121,8 +121,8 @@ class PipleLine(BOBase):
         if auto_optimizer:
             self.auto_optimizer_type = auto_optimizer_type
             if auto_optimizer_type == 'learned':
-                self.source_workloadL = ['sysbench', 'oltpbench_twitter', 'job', 'tpch']
-                self.source_workloadL.remove(hold_out_workload)
+                self.source_workloadL = ['sysbench', 'twitter', 'job', 'tpch']
+                # self.source_workloadL.remove(hold_out_workload)
                 self.ranker = xgb.XGBRanker(
                 # tree_method='gpu_hist',
                 booster='gbtree',
@@ -135,7 +135,7 @@ class PipleLine(BOBase):
                 n_estimators=110,
                 subsample=0.75
                 )
-                self.ranker.load_model("tools/xgboost_test_{}.json".format(hold_out_workload))
+                self.ranker.load_model("tools/xgboost_test_all.json".format(hold_out_workload))
                 self.history_workload_data =  history_workload_data
             elif auto_optimizer_type == 'best':
                 # self.best_method_id_list = [3] * 50 + [1] * 150
