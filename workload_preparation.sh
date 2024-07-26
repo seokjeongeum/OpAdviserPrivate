15c9dc8104df7f9f88e6eb14141c574cdadfd9e5
jeseok <jeseok@dblab.postech.ac.kr>
2024-07-26 10:52:17 +0000

    Fix docker-compose.yml

--- workload_preparation.sh
+++ workload_preparation.sh
@@ -1,5 +1,5 @@
-# Run "docker cp devcontainer-opadviser-1:/var/lib/mysql /mnt/sdc/jeseok2/mysql" in host
-chown -R mysql:mysql /var/lib/mysql
+# Run "docker cp devcontainer-opadviser-1:/var/lib/mysql /mnt/sdc/jeseok2/mysql2" in host
+#chown -R mysql:mysql /var/lib/mysql
 service mysql start
 mysql -e"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';"
 mysql -ppassword -e"create database tatp;"
