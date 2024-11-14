2103528d0b0f93276e7dddb310556cfc1505118a
root <jeseok@dblab.postech.ac.kr>
2024-11-14 14:07:27 +0900

    Fix cluster scripts

--- job.sh
+++ job.sh
@@ -4,10 +4,10 @@ cd ~/jeseok || exit
 rm -r join-order-benchmark
 git clone https://github.com/seokjeongeum/join-order-benchmark.git
 cd join-order-benchmark || exit
-mv ./csv_files/schematext.sq ./csv_files/schematext_backup.sq
+mv ./csv_files/schematext.sql ./csv_files/schematext_backup.sql
 wget http://homepages.cwi.nl/~boncz/job/imdb.tgz
 sudo tar -zxvf imdb.tgz -C csv_files
-mv ./csv_files/schematext_backup.sq ./csv_files/schematext.sq
+mv ./csv_files/schematext_backup.sql ./csv_files/schematext.sql
 source csv_files/schematext.sql
 source csv_files/fkindexes.sql
 ./load_data_mysql.sh
