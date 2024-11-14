7bd1c7eebddcde7b50ebd340914a35550cf3cff4
석정음 SEOK Jeongeum <jeseok@dblab.postech.ac.kr>
2024-11-14 15:14:37 +0900

    Fix cluster scripts

--- job.sh
+++ job.sh
@@ -4,10 +4,10 @@ cd ~/jeseok || exit
 rm -r join-order-benchmark
 git clone https://github.com/seokjeongeum/join-order-benchmark.git
 cd join-order-benchmark || exit
-mv ./csv_files/schematext.sql ./csv_files/schematext_backup.sql
+mv -f ./csv_files/schematext.sql ./csv_files/schematext_backup.sql
 wget http://homepages.cwi.nl/~boncz/job/imdb.tgz
 sudo tar -zxvf imdb.tgz -C csv_files
-mv ./csv_files/schematext_backup.sql ./csv_files/schematext.sql
+mv -f ./csv_files/schematext_backup.sql ./csv_files/schematext.sql
 mysql -u root -ppassword imdbload < csv_files/schematext.sql
 mysql -u root -ppassword imdbload < csv_files/fkindexes.sql
 ./load_data_mysql.sh
