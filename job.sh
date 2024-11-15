c68caa5a63735b75f45adc301e2ccc0b26174b20
석정음 SEOK Jeongeum <jeseok@dblab.postech.ac.kr>
2024-11-15 07:46:00 +0000

    Fix JOB script

--- job.sh
+++ job.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
-cd ~/jeseok || exit
 rm -r join-order-benchmark
 git clone https://github.com/seokjeongeum/join-order-benchmark.git
 cd join-order-benchmark || exit
 mv -f ./csv_files/schematext.sql ./csv_files/schematext_backup.sql
+apt install wget
 wget http://homepages.cwi.nl/~boncz/job/imdb.tgz
 sudo tar -zxvf imdb.tgz -C csv_files
 mv -f ./csv_files/schematext_backup.sql ./csv_files/schematext.sql
