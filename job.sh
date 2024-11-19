acaa7235d6ef5954e4c49272a7313a40cf9adfe8
Seok, Jeongeum <jeseok@dblab.postech.ac.kr>
2024-11-19 08:21:00 +0000

    Fix job.sh

--- job.sh
+++ job.sh
@@ -2,7 +2,7 @@
 cd join-order-benchmark || git clone https://github.com/seokjeongeum/join-order-benchmark.git
 cd join-order-benchmark || exit
 mv -f ./csv_files/schematext.sql ./csv_files/schematext_backup.sql
-apt install wget
+apt install -y wget
 wget http://homepages.cwi.nl/~boncz/job/imdb.tgz
 sudo tar -zxvf imdb.tgz -C csv_files
 mv -f ./csv_files/schematext_backup.sql ./csv_files/schematext.sql
