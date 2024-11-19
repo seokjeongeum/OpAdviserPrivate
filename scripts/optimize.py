16c28f58f5d02564e563770945571cae13504e7a
Seok, Jeongeum <jeseok@dblab.postech.ac.kr>
2024-11-19 23:32:40 +0000

    Fix optimize.py

--- scripts/optimize.py
+++ scripts/optimize.py
@@ -7,14 +7,14 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config', type=str, default='scripts/sysbench_rw.ini', help='config file')
+    parser.add_argument('--config', type=str, default='scripts/cluster.ini', help='config file')
     # 2024-11-19 code for clusters
-    parser.add_argument('--knob_config_file', type=str,default='scripts/experiment/gen_knobs/JOB_shap.json')
-    parser.add_argument('--knob_num', type=int,default=5)
-    parser.add_argument('--dbname', type=str,default='imdbload')
-    parser.add_argument('--workload', type=str,default='job')
+    parser.add_argument('--knob_config_file', type=str,default='scripts/experiment/gen_knobs/SYSBENCH_randomforest.json')
+    parser.add_argument('--knob_num', type=int,default=118)
+    parser.add_argument('--dbname', type=str,default='sbread')
+    parser.add_argument('--workload', type=str,default='sysbench')
     parser.add_argument('--oltpbench_config_xml', type=str,default='/oltpbench/config/sample_twitter_config.xml')
-    parser.add_argument('--task_id', type=str, default="job_ddpg_5")
+    parser.add_argument('--task_id', type=str, default="sysbench_ddpg_118")
     parser.add_argument('--optimize_method', type=str, default='DDPG')
     # 2024-11-19 code for clusters
     opt = parser.parse_args()
@@ -30,6 +30,7 @@ if __name__ == '__main__':
     args_db['oltpbench_config_xml'] = opt.oltpbench_config_xml
     args_tune['task_id'] = opt.task_id
     args_tune['optimize_method'] = opt.optimize_method
+    args_tune['initial_tunable_knob_num']=opt.knob_num
     # 2024-11-19 code for clusters
     if args_db['db'] == 'mysql':
         db = MysqlDB(args_db)
