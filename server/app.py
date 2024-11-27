4693a5de219221f139e417e7fd90f7a2cceb8c79
석정음 SEOK Jeongeum <jeseok@dblab.postech.ac.kr>
2024-11-27 04:40:24 +0000

    Fix query

--- server/app.py
+++ server/app.py
@@ -14,7 +14,7 @@ def hello_world():
         ) as f:
             f.write(
                 f"""select current_timestamp(6) into @query_start;
-set @query_name='1';
+set @query_name='{i}';
 {query}
 set @query_time_ms= timestampdiff(microsecond, @query_start, current_timestamp(6))/1000;
 SELECT @query_name, @query_time_ms;
